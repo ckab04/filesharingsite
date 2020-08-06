7ed61463549cb3cac0ce4c883beb1a0ef83cdab7
ckap04 <ciceronkapenabm@gmail.com>
2020-08-06 23:51:15 +0200

    Adding the controller and the home page

--- FileSharing/src/main/java/site/filesharing/controller/FileSharingController.java
+++ FileSharing/src/main/java/site/filesharing/controller/FileSharingController.java
@@ -1,5 +1,15 @@
 package site.filesharing.controller;
 
-public class FileSharingController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class FileSharingController {
+	
+	@GetMapping("/")
+ public String home()
+ {
+	 
+	 return "home";
+ }
 }
